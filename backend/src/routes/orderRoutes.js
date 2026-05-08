a28948745d5baf58c91938ec87e815e6dd9d5ab3
frontend-ingold <frontend1@ingoldsolutions.com>
2026-05-08 15:02:05 +0530

    Delete backend directory

--- backend/src/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createOrder, getOrdersByUser } from "../controllers/orderController.js";
-
-const router = Router();
-
-router.get("/user/:userId", getOrdersByUser);
-router.post("/", createOrder);
-
-export default router;
--- backend/src/server.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-
-const port = Number(process.env.PORT || 5000);
-
-app.listen(port, () => {
-  console.log(`Backend server running on port ${port}`);
-});
